ddbe91cb7dee49e138d943720ae46969cd83912d
unknown <arjun.tech77@gmail.com>
2024-03-14 11:50:47 +0530

    Home page work completed

--- client/src/components/ListingItems.jsx
+++ client/src/components/ListingItems.jsx
@@ -1,46 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { MdLocationOn } from 'react-icons/md'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdLocationOn } from 'react-icons/md';
 
-function ListingItems({list}) {
+function ListingItems({ list }) {
   return (
-    <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
-        <Link to={`/listing/${list._id}`}>
-        <img  src={list.imageUrls[0]}  className='h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transiction-scale duration-300' />
-         <div className="p-3">
-           <p className='text-lg font-semibold text-slate-700 truncate'>{list.name}</p>
-
-           <div className="flex items-center gap-1">
-            <MdLocationOn className='h-4 w-4 text-green-700'/>
-            <p className="text-sm text-gray-600 truncate w-full">{list.address}</p>
-           </div>
-           <p className='text-sm text-gray-600 line-clamp-2'>
-            {list.description}
-           </p>
-
-           <p className="text-slate-500 mt-2 font-semibold">₹ 
-           {list.offer && typeof list.discountPrice === 'number' 
-    ? list.discountPrice.toLocaleString('en-US')
-    : typeof list.regularPrice === 'number' 
-        ? list.regularPrice.toLocaleString('en-US')
-        : "Price not available"
-  }
-        {list.type === 'rent' && ' /month'}
-
-           </p>
-           <div className="text-slate-700 flex gap-4">
-            <div className="font-bold text-xs">
-                {list.bedrooms > 1 ? `${list.bedrooms} beds  ` : `${list.bedrooms}  bed`}
+    <div className="bg-white shadow-lg rounded-lg overflow-hidden w-full sm:w-[330px] transition duration-300 transform hover:scale-105">
+      <Link to={`/listing/${list._id}`}>
+        <div className="relative overflow-hidden">
+          <img src={list.imageUrls[0]} className="w-full h-52 sm:h-40 object-cover transition-transform duration-300 transform hover:scale-110" alt={list.name} />
+          <div className="absolute bottom-0 left-0 bg-gradient-to-t from-gray-800 to-transparent w-full h-20"></div>
+        </div>
+        <div className="p-4">
+          <p className="text-lg font-semibold text-gray-800">{list.name}</p>
+          <div className="flex items-center mt-2 text-gray-600 text-sm">
+            <MdLocationOn className="h-4 w-4 mr-1" />
+            <p>{list.address}</p>
+          </div>
+          <p className="text-sm text-gray-700 mt-2">{list.description}</p>
+          <p className="text-lg font-semibold text-green-500 mt-2">
+            {list.offer && typeof list.discountPrice === 'number'
+              ? `$${list.discountPrice.toLocaleString('en-US')}`
+              : typeof list.regularPrice === 'number'
+              ? `$${list.regularPrice.toLocaleString('en-US')}`
+              : "Price not available"
+            }
+            {list.type === 'rent' && ' /month'}
+          </p>
+          <div className="flex justify-between mt-2">
+            <div className="text-gray-700 font-semibold">
+              {list.bedrooms} {list.bedrooms > 1 ? 'beds' : 'bed'}
             </div>
-
-            <div className="font-bold text-xs">
-                {list.bathrooms > 1 ? `${list.bathrooms} baths` : `${list.bathrooms} baths`}
+            <div className="text-gray-700 font-semibold">
+              {list.bathrooms} {list.bathrooms > 1 ? 'baths' : 'bath'}
             </div>
-           </div>
-         </div>
-        </Link>
+          </div>
+        </div>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default ListingItems
+export default ListingItems;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
+import ListingItems from '../components/ListingItems';
 
-function Home() {
+export default function Home() {
+  const [offerListings, setOfferListings] = useState([]);
+  const [saleListings, setSaleListings] = useState([]);
+  const [rentListings, setRentListings] = useState([]);
+  
+  SwiperCore.use([Navigation]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const offerRes = await fetch('/api/listing/get?offer=true&limit=4');
+        const offerData = await offerRes.json();
+        setOfferListings(offerData);
+        
+        const rentRes = await fetch('/api/listing/get?type=rent&limit=4');
+        const rentData = await rentRes.json();
+        setRentListings(rentData);
+        
+        const saleRes = await fetch('/api/listing/get?type=sale&limit=4');
+        const saleData = await saleRes.json();
+        setSaleListings(saleData);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>Home</div>
-  )
-}
+    <div className="bg-gradient-to-b from-green-950 via-green-900 to-black-900 min-h-screen">
+      {/* Top section */}
+      <div className="container mx-auto px-4 py-12 text-center">
+        <h1 className="text-4xl lg:text-6xl font-bold text-white mb-4">
+          Find your next <span className="text-yellow-400">perfect</span> place with ease
+        </h1>
+        <p className="text-lg text-white mb-8">
+          Sahand Estate is the best place to find your next perfect place to live. We have a wide range of properties for you to choose from.
+        </p>
+        <Link to="/search" className="text-lg bg-yellow-400 py-2 px-6 rounded-full font-semibold hover:bg-yellow-500 hover:text-white transition duration-300 ease-in-out inline-block">Let's get started...</Link>
+      </div>
 
-export default Home
+      {/* Offer swiper */}
+      <Swiper navigation>
+        {offerListings.map(listing => (
+          <SwiperSlide key={listing._id}>
+            <div className="h-72 bg-cover bg-center rounded-md" style={{ backgroundImage: `url(${listing.imageUrls[0]})` }} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Listing results */}
+      <div className="container mx-auto px-4 py-12">
+        <div className="mb-12">
+          <h2 className="text-2xl font-semibold text-white mb-4">Recent offers</h2>
+          <Link to="/search?offer=true" className="text-sm bg-yellow-400 py-2 px-4 rounded-full font-semibold hover:bg-yellow-500 hover:text-white transition duration-300 ease-in-out inline-block">Show more offers</Link>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {offerListings.map(listing => (
+            <ListingItems key={listing._id} list={listing} />
+          ))}
+        </div>
+
+        {/* Rent listings */}
+        <div className="mt-12 mb-8">
+          <h2 className="text-2xl font-semibold text-white mb-4">Recent places for rent</h2>
+          <Link to="/search?type=rent" className="text-sm bg-yellow-400 py-2 px-4 rounded-full font-semibold hover:bg-yellow-500 hover:text-white transition duration-300 ease-in-out inline-block">Show more places for rent</Link>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {rentListings.map(listing => (
+            <ListingItems key={listing._id} list={listing} />
+          ))}
+        </div>
+
+        {/* Sale listings */}
+        <div className="mt-12 mb-8">
+          <h2 className="text-2xl font-semibold text-white mb-4">Recent places for sale</h2>
+          <Link to="/search?type=sale" className="text-sm bg-yellow-400 py-2 px-4 rounded-full font-semibold hover:bg-yellow-500 hover:text-white transition duration-300 ease-in-out inline-block">Show more places for sale</Link>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {saleListings.map(listing => (
+            <ListingItems key={listing._id} list={listing} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
