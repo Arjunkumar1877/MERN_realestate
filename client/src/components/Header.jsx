f9101fb23b1473886f839aadf7709eee1f14694f
Arjun kumar vs <132894618+Arjunkumar1877@users.noreply.github.com>
2024-08-29 20:46:41 +0000

    added a border for the profile avatar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -52,7 +52,9 @@ function Header() {
        </Link>
        <Link to="/profile">
          {currentUser ? (
-<img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="" />
+<div className="border-2 border-slate-300 rounded-full shadow-inner">
+<img className='rounded-full h-7 w-7 object-cover' src={currentUser?.avatar} alt="" />
+</div>
          ) : (
        <li className='hidden sm:inline text-slate-700 hover:underline'>Sign in</li>
          )
